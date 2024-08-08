50c8c596c8c6b2ee3c424da170cef9a169ad5d04
Parvinder Singh <parvwebdev03@gmail.com>
2024-08-08 09:26:48 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using node.js, not browser
+
+console.log(3
+    +
+    3)    //code readability should be high
+
+console.log("Sahej")
+
+
+let name = "Sahej"
+let age = 19
+let isLoggedin = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined);
