b041ba4a72eb4110988f652976f6b0a53efc90da
Parvinder Singh <parvwebdev03@gmail.com>
2024-08-09 05:10:43 +0000

    3 conversion done

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,14 @@
+let score = "sahej"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber); 
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => nan => not a number
+// true = 1; false = 0;
+
